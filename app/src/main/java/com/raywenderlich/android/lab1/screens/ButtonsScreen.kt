209a08d4d1ce5188e0a7b6c3178f842d0393d1f2
JvovL <jjvovo715@gmail.com>
2024-02-13 17:41:24 +0300

    3-1

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
@@ -41,7 +44,12 @@ fun MyButton() {
             1.dp,
             color = colorResource(id = R.color.black)
         )
-    
+    ){
+        Text(
+            text = stringResource(id = R.string.set_button),
+            color = Color.White
+        )
+    }
 }
 
 @Composable
--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.ComposableTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.stringResource;
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
