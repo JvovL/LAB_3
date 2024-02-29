2a0603a48780971575144e22fb9a65960bead20c
JvovL <jjvovo715@gmail.com>
2024-03-01 00:08:55 +0300

    GridScreen 3-3-2

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -40,7 +40,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_Surface), Screen.Surface)
             NavigationButton(stringResource(id = R.string.button_Scaffold), Screen.Scaffold)
             NavigationButton(stringResource(id = R.string.button_Scrolling), Screen.Scrolling)
-            NavigationButton(stringResource(id = R.string.button_Lists), Screen.Lists)
+            NavigationButton(stringResource(id = R.string.button_Lists), Screen.List)
             NavigationButton(stringResource(id = R.string.button_Grid), Screen.Grid)
         }
     }
